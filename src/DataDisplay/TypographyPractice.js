c7db673fa1b4a1a83326eccc1fff20382760fcde
ABlued <53801395+ABlued@users.noreply.github.com>
2022-02-02 00:18:30 +0900

    Mui Typography 실습

---
+++ src/DataDisplay/TypographyPractice.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+function TypographyPractice() {
+  return (
+    <>
+      <Typography variant="h1">Heading1</Typography>
+      <Typography variant="h2">Heading2</Typography>
+      <Typography variant="h3">Heading3</Typography>
+      <Typography variant="body1">body1</Typography>
+      <Typography variant="body2">body2</Typography>
+    </>
+  );
+}
+
+export default TypographyPractice;
